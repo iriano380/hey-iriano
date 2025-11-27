08a2887006bff30849f472d06de1651cf36c2733
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-27 14:35:41 +0200

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -7,25 +7,6 @@ const SuggestionCard = ({ text }: { text: string }) => (
   </div>
 );
 
-/* Link moderno */
-const ExternalLink = ({
-  children,
-  href,
-}: {
-  children: React.ReactNode;
-  href: string;
-}) => {
-  return (
-    <NextLink
-      target="_blank"
-      className="text-blue-500 hover:text-blue-600 transition-colors duration-75"
-      href={href}
-    >
-      {children}
-    </NextLink>
-  );
-};
-
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
