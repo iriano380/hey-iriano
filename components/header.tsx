64f3a76adbae4ea31c19a59343b547f44884c525
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-27 15:40:23 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,6 @@ export const Header = () => {
       <div className="flex justify-between items-center p-4">
         <div className="flex flex-row items-center gap-2 shrink-0 ">
           <span className="jsx-e3e12cc6f9ad5a71 flex flex-row items-center gap-2 home-links">
-
             <Link
               className="text-zinc-800 dark:text-zinc-100 -translate-y-[.5px]"
               rel="noopener"
@@ -31,18 +30,30 @@ export const Header = () => {
                 />
               </svg>
             </Link>
-
-            {/* TRAÇO REMOVIDO AQUI */}
-
+            <div className="jsx-e3e12cc6f9ad5a71 w-4 text-lg text-center text-zinc-300 dark:text-zinc-600">
+              <svg
+                data-testid="geist-icon"
+                height={16}
+                strokeLinejoin="round"
+                viewBox="0 0 16 16"
+                width={16}
+                style={{ color: "currentcolor" }}
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4.01526 15.3939L4.3107 14.7046L10.3107 0.704556L10.6061 0.0151978L11.9849 0.606077L11.6894 1.29544L5.68942 15.2954L5.39398 15.9848L4.01526 15.3939Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </div>
             <div className="jsx-e3e12cc6f9ad5a71 flex flex-row items-center gap-4">
               <Link className="flex flex-row items-end gap-2" target="_blank" href="https://groq.com">
                 <GroqIcon size={32} />
               </Link>
             </div>
-
           </span>
         </div>
-
         <div className="flex flex-row items-center gap-2 shrink-0">
           <DeployButton />
         </div>
