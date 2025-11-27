32a6c6284fff0f9378fc8aeaf15fc4d0f15445e7
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-27 14:20:03 +0200

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -2,12 +2,12 @@ import NextLink from "next/link";
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
-      <h1 className="text-3xl font-semibold mb-4">Em que posso ajudar hoje?</h1>
+      <h1 className="text-3xl font-semibold mb-4">Vercel x Groq Chatbot</h1>
       <p className="text-center">
-        Olá eu sou o "Hey hiriano" o seu assistente pessoal para quase tudo... Fui criado pela <Link href="https://groq.com/">IrianoCreative-Studio</Link>{" "}
-        Em Moçambique/Vilanculo siga-me no <Link href="https://sdk.vercel.ai/docs">Facebook</Link> para poder{" "}
+        This starter project uses <Link href="https://groq.com/">Groq</Link>{" "}
+        with the <Link href="https://sdk.vercel.ai/docs">AI SDK</Link> via the{" "}
         <Link href="https://vercel.com/marketplace/groq">
-          Obter mais ajuda
+          Vercel Marketplace
         </Link>
         .
       </p>
