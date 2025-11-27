43a7f06c0c0ca12cd732dce2a24983e2a7a36b37
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-27 14:18:18 +0200

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -2,12 +2,12 @@ import NextLink from "next/link";
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
-      <h1 className="text-3xl font-semibold mb-4">Em que posso ajudar?</h1>
+      <h1 className="text-3xl font-semibold mb-4">Em que posso ajudar hoje?</h1>
       <p className="text-center">
-        Olá eu sou o "Hey hiriano" o seu assistente gratuito... Descubra mais IAs como eu em<Link href="https://groq.com/">IrianoCreative-Studios</Link>{" "}
-        E pode também me seguir <Link href="https://sdk.vercel.ai/docs">Clicando aqui</Link> via the{" "}
+        Olá eu sou o "Hey hiriano" o seu assistente pessoal para quase tudo... Fui criado pela <Link href="https://groq.com/">IrianoCreative-Studio</Link>{" "}
+        Em Moçambique/Vilanculo siga-me no <Link href="https://sdk.vercel.ai/docs">Facebook</Link> para poder{" "}
         <Link href="https://vercel.com/marketplace/groq">
-          Vercel Marketplac
+          Obter mais ajuda
         </Link>
         .
       </p>
