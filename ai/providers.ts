41da3bf4a0fca54444ec8b61e3b2df8a5eb2a8ef
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-27 16:57:17 +0200

    Update providers.ts

--- ai/providers.ts
+++ ai/providers.ts
@@ -3,33 +3,25 @@ import {
   customProvider,
   extractReasoningMiddleware,
   wrapLanguageModel,
-  ImageModel,
 } from "ai";
 
-// Modelos de texto
 const languageModels = {
   "kimi-k2": groq("moonshotai/kimi-k2-instruct"),
   "meta-llama/llama-4-scout-17b-16e-instruct": groq(
-    "meta-llama/llama-4-scout-17b-16e-instruct"
+    "meta-llama/llama-4-scout-17b-16e-instruct",
   ),
   "llama-3.1-8b-instant": groq("llama-3.1-8b-instant"),
   "deepseek-r1-distill-llama-70b": wrapLanguageModel({
-    middleware: extractReasoningMiddleware({ tagName: "think" }),
+    middleware: extractReasoningMiddleware({
+      tagName: "think",
+    }),
     model: groq("deepseek-r1-distill-llama-70b"),
   }),
   "llama-3.3-70b-versatile": groq("llama-3.3-70b-versatile"),
 };
 
-// Modelos de imagem
-const imageModels = {
-  "dalle-mini": new ImageModel({
-    model: groq("openai/dalle-mini"),
-  }),
-};
-
 export const model = customProvider({
   languageModels,
-  imageModels,
 });
 
 export type modelID = keyof typeof languageModels;
