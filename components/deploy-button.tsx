19375871ad9a606125ec21222c689d2fb085e4e3
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-28 16:44:41 +0200

    Update deploy-button.tsx

--- components/deploy-button.tsx
+++ components/deploy-button.tsx
@@ -5,22 +5,16 @@ export const DeployButton = () => (
     href={`https://hey-iriano.vercel.app`}
     target="_blank"
     rel="noopener noreferrer"
-    className="inline-flex items-center gap-2 ml-2 bg-black text-white text-sm px-3 py-1.5 rounded-md hover:bg-zinc-900 dark:bg-white dark:text-black dark:hover:bg-zinc-100"
+    className="inline-flex items-center gap-2 ml-2 text-white text-sm px-3 py-1.5 rounded-md transition-colors duration-200 hover:bg-zinc-900"
+    style={{ backgroundColor: "#212832" }}
   >
     <svg
-      data-testid="geist-icon"
-      height={14}
-      strokeLinejoin="round"
-      viewBox="0 0 16 16"
-      width={14}
-      style={{ color: "currentcolor" }}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="white"
+      className="w-4 h-4"
     >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M8 1L16 15H0L8 1Z"
-        fill="currentColor"
-      />
+      <path d="M11 11V7H13V11H17V13H13V17H11V13H7V11H11ZM12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z" />
     </svg>
     Novo chat
   </Link>
