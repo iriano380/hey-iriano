cace6be848f810f137ee6b2c9111582ae43f0c41
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-28 13:25:37 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Header } from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,8 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Hey hiriano",
-  description:
-    "Seu assistante virtuail",
+  description: "Seu assistante virtuail",
 };
 
 export default function RootLayout({
@@ -25,11 +25,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
+      <head>
+        {/* RemixIcon CDN (funciona sem instalar nada) */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css"
+          rel="stylesheet"
+        />
+      </head>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* Cabeçalho */}
+        <Header />
+
+        {/* Conteúdo */}
+        <main className="pt-20">{children}</main>
+
         <Toaster />
       </body>
     </html>
