c2c931f3ff40eba15e6bfe6e0f8f5ae2bded46c1
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-11-28 15:33:50 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,11 @@ import { DeployButton } from "./deploy-button";
 
 export const Header = () => {
   return (
-    <div className="fixed right-0 left-0 top-4 mx-4 rounded-xl bg-white/30 dark:bg-zinc-950/30 backdrop-blur-md shadow-lg z-50">
-      <div className="flex justify-between items-center p-4">
+    <div
+      className="fixed right-0 left-0 top-4 mx-4 rounded-xl bg-white/30 dark:bg-zinc-950/30 backdrop-blur-md shadow-lg z-50 border"
+      style={{ borderColor: "#212832" }}
+    >
+      <div className="flex justify-between items-center py-2 px-4">
         {/* Logo */}
         <div className="flex flex-row items-center gap-2 shrink-0">
           <Link href="/" className="flex items-center">
