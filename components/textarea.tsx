d8ebbc65e92b0f1b8b74e654e091c166a637f7d2
IrianoCreative Angecy <Xoonashop@gmail.com>
2025-12-05 09:31:21 +0200

    Update textarea.tsx

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -60,13 +60,13 @@ export const Textarea = ({
                 cx="12"
                 cy="12"
                 r="10"
-                stroke="currentColor"
+                stroke="#333"
                 strokeWidth="4"
                 fill="none"
               />
               <path
                 className="opacity-75"
-                fill="currentColor"
+                fill="orange"
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
               />
             </svg>
